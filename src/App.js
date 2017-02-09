490d1c93e0f634df933d1008227ddfa8512c2aa2
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-08 19:37:22 -0800

    Generate skeleton with create-react-app and add some temp content

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </div>
+        <div className="Todo-App">
+          <form>
+            <input type="text" />
+          </form>
+          <div className="Todo-List">
+            <ul>
+              <li><input type="checkbox" />Learn JsX</li>
+              <li><input type="checkbox" />Build an awesome app</li>
+              <li><input type="checkbox" />Ship it</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
