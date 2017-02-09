37178292557a8ce4520378a2d7cccdfc67c25ea3
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-08 22:33:29 -0800

    Add more helper functions and start adding tests

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -1,3 +1,9 @@
 export const addTodo = (todos, newTodo) => [...todos, newTodo];
 
 export const generateId = () => Math.floor(Math.random()*90000) + 10000;
+
+export const findById = (id, array) => {
+  return array.find((todo) => {
+    return todo.id === id;
+  });
+};
--- src/lib/todoHelpers.test.js
+++ src/lib/todoHelpers.test.js
@@ -1,4 +1,4 @@
-import {addTodo, generateId} from './todoHelpers';
+import {addTodo, generateId, findById} from './todoHelpers';
 
 test('addTodo should add the passed todo to the list', () => {
   const startTodos = [
@@ -31,4 +31,32 @@ test('generateId should return a number', () => {
   const id = generateId();
   expect(typeof(id)).toBe('number');
   expect(id.toString().length).toEqual(5);
+});
+
+test('findById should return the expected item from an array', () => {
+  const startTodos = [
+    {id: 1, name: 'one', isComplete: false},
+    {id: 2, name: 'two', isComplete: false},
+    {id: 3, name: 'three', isComplete: false}
+  ];
+  const expected = {id: 2, name: 'two', isComplete: false};
+
+  const result = findById(2, startTodos);
+  expect(result).toEqual(expected);
+});
+
+test.skip('toggleTodo should toggle the isComplete prop of a todo', () => {
+
+});
+
+test.skip('toggleTodo should not mutate the original todo', () => {
+
+});
+
+test.skip('updateTodo should update an item by id', () => {
+
+});
+
+test.skip('updateTodo should not mutate the original array', () => {
+
 });
