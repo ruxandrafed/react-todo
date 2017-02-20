d220fcae4c337a39afd955f3fbd15389cfd3b7a8
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-19 19:39:38 -0800

    Add pipe function that does function compositions (any no. of functions)

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,11 @@
-export const partial = (func, ...args) => {
+export const partial = (fn, ...args) => {
   // ...args ^^^ takes multiple arguments and combines them in an array
-  return func.bind(null, ...args);
+  return fn.bind(null, ...args);
   /// ...args ^^^ we spread the array back into arguments
 };
+
+// does function composition
+const _pipe = (f, g) => (...args) => g(f(...args));
+
+// takes an array of functions
+export const pipe = (...fns) => fns.reduce(_pipe);
